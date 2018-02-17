d68dec6e5ce7cbd7866d1260a511f5c1e5726568
satishrodd <rod.satish@gmail.com>
2018-02-17 15:47:32 +0000

    insert sort

--- insert_srt.c
+++ insert_srt.c
@@ -6,10 +6,28 @@
  *
  *     <pi@>
  *
- *     Copyright (c) 2018 RedBack Networks, Inc.
  *     All rights reserved.
  *
  *---------------------------------------------------------------------------
  */
+#include<stdio.h>
+#include"data.h"
+
+void main() {
+    int i, j, key;
+
+    for (j=1;j<n;j++) {
+        key = a[j];
+        i = j-1;
+        while (i>=0 && a[i]>key) {
+            a[i+1] = a[i];
+            i = i-1;
+        }
+        a[i+1] = key;
+    }
+    for ( i=0 ;i<n ;i++ ) {
+        printf("%d ", a[i]);
+    }
+}
 
 
